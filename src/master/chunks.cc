1fc8cbed98fa78fe6b1425edd5fd5f86fadc47fe
guillex <guillex@leil.io>
2025-08-22 17:43:42 +0200

    feat(master): Emit signals on Chunk changes
    Signed-off-by: guillex <guillex@leil.io>

--- src/master/chunks.cc
+++ src/master/chunks.cc
@@ -728,6 +728,10 @@ static void chunk_recalculate_checksum() {
 	}
 }
 
+static inline void emit_chunk_changed(const Chunk *c) {
+	gChunkChangedSignal.emit(c->chunkid, c->version, c->lockedto, c->lockid);
+}
+
 uint64_t chunk_checksum(ChecksumMode mode) {
 	uint64_t checksum = 46586918175221;
 	addToChecksum(checksum, gChunksMetadata->nextchunkid);
@@ -799,6 +803,7 @@ void chunk_emergency_increase_version(Chunk *c) {
 	c->version++;
 	chunk_update_checksum(c);
 	fs_incversion(c->chunkid);
+	emit_chunk_changed(c);
 }
 
 void chunk_finalize_failed_operation(Chunk *c) {
@@ -1006,6 +1011,7 @@ int chunk_unlock(uint64_t chunkid) {
 	// Don't remove lockid to safely accept retransmission of FUSE_CHUNK_UNLOCK message
 	c->lockedto = 0;
 	chunk_update_checksum(c);
+	emit_chunk_changed(c);
 	return SAUNAFS_STATUS_OK;
 }
 
@@ -1190,6 +1196,7 @@ uint8_t chunk_multi_modify(uint64_t ochunkid, uint32_t *lockid, uint8_t goal,
 	}
 	c->lockid = *lockid;
 	chunk_update_checksum(c);
+	emit_chunk_changed(c);
 	return SAUNAFS_STATUS_OK;
 }
 
@@ -1268,6 +1275,7 @@ uint8_t chunk_multi_truncate(uint64_t ochunkid, uint32_t lockid, uint32_t length
 	c->lockedto=(uint32_t)eventloop_time()+LOCKTIMEOUT;
 	c->lockid = lockid;
 	chunk_update_checksum(c);
+	emit_chunk_changed(c);
 	return SAUNAFS_STATUS_OK;
 }
 #endif // ! METARESTORE
@@ -1302,6 +1310,7 @@ uint8_t chunk_apply_modification(uint32_t ts, uint64_t oldChunkId, uint32_t lock
 	c->lockedto = ts + LOCKTIMEOUT;
 	c->lockid = lockid;
 	chunk_update_checksum(c);
+	emit_chunk_changed(c);
 	*newChunkId = c->chunkid;
 	return SAUNAFS_STATUS_OK;
 }
@@ -1374,6 +1383,7 @@ int chunk_repair(uint8_t goal, uint64_t ochunkid, uint32_t *nversion, uint8_t co
 	c->needverincrease=1;
 	c->updateStats();
 	chunk_update_checksum(c);
+	emit_chunk_changed(c);
 	return 1;
 }
 #endif
@@ -1387,6 +1397,7 @@ int chunk_set_version(uint64_t chunkid,uint32_t version) {
 	}
 	c->version = version;
 	chunk_update_checksum(c);
+	emit_chunk_changed(c);
 	return SAUNAFS_STATUS_OK;
 }
 
@@ -1399,6 +1410,7 @@ int chunk_increase_version(uint64_t chunkid) {
 	}
 	c->version++;
 	chunk_update_checksum(c);
+	emit_chunk_changed(c);
 	return SAUNAFS_STATUS_OK;
 }
 
@@ -1541,6 +1553,7 @@ void chunk_server_has_chunk(matocsserventry *ptr, uint64_t chunkid, uint32_t ver
 		c->lockedto = (uint32_t)eventloop_time()+UNUSED_DELETE_TIMEOUT;
 		c->lockid = 0;
 		chunk_update_checksum(c);
+		emit_chunk_changed(c);
 	}
 	auto server_csid = matocsserv_get_csdb(ptr)->csid;
 	for (auto &part : c->parts) {
@@ -2707,6 +2720,13 @@ void chunk_dump(void) {
 }
 
 #endif
+
+void chunk_add_from_initial_metadata_load(uint64_t chunkId, uint32_t chunkVersion,
+                                          uint32_t lockedTo, uint32_t lockId) {
+	Chunk *chunk = chunk_new(chunkId, chunkVersion);
+	chunk->lockedto = lockedTo;
+	chunk->lockid = lockId;
+}
 
 bool chunksLoadFromFile(MetadataLoader::Options options) {
 	const uint8_t *ptr = options.metadataFile->seek(options.offset);
